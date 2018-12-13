509dba06d7858526c7a20774d6a99b1f3f31ce70
PasikaSanguansab <43163999+PasikaSanguansab@users.noreply.github.com>
2018-12-13 17:38:40 +0700

    update

--- Coding/Height Weight.py
+++ Coding/Height Weight.py
@@ -22,6 +22,25 @@ def main():
             i = i.replace("kg", "")
         weight_convert.append(int(i))
     player_list = {}
+    for i in range(len(player)):
+        player_list[player[i]] = [height_convert[i], weight_convert[i]]
+    top_h = sorted(player_list.items(), key=lambda x: x[1][0], reverse = True)
+    top_w = sorted(player_list.items(), key=lambda x: x[1][1], reverse = True)
+    chartmaker(top_h, 30, "Height")
+    chartmaker(top_h, 5, "Height")
+    chartmaker(top_w, 30, "Weight")
+    chartmaker(top_w, 5, "Weight")
+    
+    
+def chartmaker(top, num, title):
+    """Chart making function"""
+    check = 0 if title == "Height" else 1
+    check1 = "cm" if title == "Height" else "kg"
+    line_chart = pg.Bar(legend_at_bottom=True, legend_at_bottom_columns=6)
+    line_chart.title = "Top %d the most %s players (%s)" %(num, title, check1)
+    for key, value in top[:num]:
+        line_chart.add(str(key), value[check])
+    line_chart.render_to_file('pic/Top %d the most %s players.svg' %(num, title))
 
 
 def convert_h(i):
